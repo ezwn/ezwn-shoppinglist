01985118b9ccff7e7b97d3eb89fa7c3835a402e8
Nicolas ENZWEILER <nicolas.enzweiler@gmail.com>
2020-04-27 12:28:02 +0200

    FEATURE : improve 'add article'

--- features/AddArticle-ctx.js
+++ features/AddArticle-ctx.js
@@ -2,20 +2,29 @@ import React, { useState, useContext } from "react";
 import { useHistory } from "react-router";
 
 import { ArticleListContext } from "./ArticleList-ctx";
+import { ShoppingListContext } from "./ShoppingList-ctx";
 
 export const AddArticleContext = React.createContext();
 
 export const AddArticleProvider = ({ children }) => {
     const history = useHistory();
     const [article, setArticle] = useState({ name: '' });
-    const { addArticle } = useContext(ArticleListContext);
+    const { addArticle, articleList } = useContext(ArticleListContext);
+    const { addShoppingListItem } = useContext(ShoppingListContext);
+
+    const valid = article.name.trim().length >= 1;
 
     async function submit() {
-        await addArticle(article);
-        history.goBack();
+        if (valid) {
+            if (!articleList.find(art => art.name === article.name)) {
+                await addArticle(article);
+            }
+            await addShoppingListItem(article.name);
+            history.goBack();
+        }
     }
 
-    return <AddArticleContext.Provider value={{ article, setArticle, submit }}>
+    return <AddArticleContext.Provider value={{ article, setArticle, submit, valid }}>
         {children}
     </AddArticleContext.Provider>;
 }
